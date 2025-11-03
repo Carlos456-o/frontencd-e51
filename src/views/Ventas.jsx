c474b024a597e2d9d589946942f6ab6329ff341f
carlos oporta <elcalus456@gmail.com>
2025-11-03 09:40:45 -0600

    algunas tablas

--- src/views/Ventas.jsx
+++ src/views/Ventas.jsx
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import {Container, Col, Row, Button} from 'react-bootstrap';
 import TablaVentas from "../components/ventas/TablaVentas";
+import CuadroBusquedas from "../components/busquedas/CuadroBusquedas";
 
 const Ventas = () => {
+
+  const [ventasFiltradas, setventasFiltradas] = useState([]);
+  const [textoBusqueda, setTextoBusqueda] = useState("");
+
+
   const [ventas, setVentas] = useState([]);
   const [cargando, setCargando] = useState(true);
   const obtenerVentas = async () => {
@@ -13,12 +19,27 @@ const Ventas = () => {
       }
       const datos = await respuesta.json();
       setVentas(datos);
+      setventasFiltradas(datos);
       setCargando(false);
     } catch (error) {
       console.long(error.message);
       setCargando(false);
     }
   }
+
+  const manejarCambioBusqueda = (e) => {
+  const texto = e.target.value.toLowerCase();
+  setTextoBusqueda(texto);
+  
+  const filtradas = ventas.filter(
+    (venta) =>
+      venta.id_cliente == texto ||
+      venta.id_empleado == texto ||
+      venta.fecha_venta == texto ||
+      venta.total_venta == texto
+  );
+  setventasFiltradas(filtradas);
+};
 
   useEffect(() => {
     obtenerVentas();
@@ -27,8 +48,18 @@ const Ventas = () => {
     <>
     <Container className="mt-4">
         <h4>Ventas</h4>
+
+
+
+            <CuadroBusquedas
+              textoBusqueda={textoBusqueda}
+              manejarCambioBusqueda={manejarCambioBusqueda}
+            />
+
+
+
         <TablaVentas 
-        ventas={ventas} 
+        ventas={ventasFiltradas} 
         cargando={cargando}
         />
     </Container>
